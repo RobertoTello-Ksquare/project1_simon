2511ee9e91439782416e764cc0e6a121a60738af
Rodrigo Leon <rodrigo.leon@itksquare.edu.mx>
2022-09-24 16:56:56 -0500

    Tile sounds added

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const colors = ["red", "blue", "yellow", "green"];
 
 //Selecting Elements
 const startButton = document.querySelector(".start");
+const box = document.querySelector(".box");
 
 //Functions
 //Activate tile
 function activateTile(color) {
   const tile = document.querySelector(`[data-color='${color}']`);
 
+  const sound = document.querySelector(`[data-sound='${color}']`);
+
   tile.classList.add("activated");
+  sound.play();
 
   setTimeout(() => {
     tile.classList.remove("activated");
@@ -43,8 +47,22 @@ function nextRound() {
   playRound(nextSequence);
 }
 
+//Recive the user clicks
+function handleClick(color) {
+  const index = playerSequence.push(color) - 1;
+  const sound = document.querySelector(`[data-sound='${color}']`);
+  sound.play();
+}
 //Start Game
 function startGame() {
   nextRound();
 }
+
+//Events
 startButton.addEventListener("click", startGame);
+
+box.addEventListener("click", (event) => {
+  const { color } = event.target.dataset;
+
+  if (color) handleClick(color);
+});
